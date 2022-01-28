9c803c86a50c4671493682c4952366cab9138d46
savasto <savastova.olga@gmail.com>
2022-01-28 12:26:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//Demanar a l’usuari 6 números i guardar-los en una taula. Mostrar a pantalla la taula en l’ordre invertit:
+//
+//Entrada: 1 4 6 8 9 2
+//
+//Resultat: 2 9 8 6 4 1
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[6];
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) { // ir pediendo 6 numeros
+            System.out.println("Print numero "+ i);
+            int numero = sc.nextInt();// leemos el numero
+            numeros [i] = numero;// guardamos en la posicio i
+        }
+
+        for (int i = numeros.length-1; i >= 0; i--) {
+            System.out.println (numeros[i]);
+
+        }
+    }
+}
